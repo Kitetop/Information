8ab6b6dd04ea6d17c4f19c86f4b1de36b4ccf055
Kitetop <136321599@qq.com>
2018-08-08 03:58:53 +0100

    完善出错处理机制

--- app/Service/Yivian/CollectionYivian.php
+++ app/Service/Yivian/CollectionYivian.php
@@ -2,6 +2,7 @@
 
 namespace App\Service\Yivian;
 
+use App\Service\Exc;
 use Mx\Service\ServiceAbstract;
 use QL\QueryList;
 use App\Biz\News;
@@ -19,13 +20,16 @@ class CollectionYivian extends ServiceAbstract
     {
         // TODO: Implement execute() method.
         //给每一个爬取文章设置规则
-        $rules = require __DIR__. '/config.php';
+        $rules = require __DIR__ . '/config.php';
         $news = new News();
         //$service是一个二维数组，存储了提取值的url列表以及标题
         $service = $this->call('Collection\GrabSource', [
             'url' => $this->url,
             'rules' => $this->rules,
         ]);
+        if (!isset($service)) {
+            throw new Exc('采集服务出错',500);
+        }
         foreach ($service as $key => $value) {
             $news = $news->dao()->findOne(['url' => $value['url'], 'name' => 'yivian']);
             if (true == $news->exist()) {
